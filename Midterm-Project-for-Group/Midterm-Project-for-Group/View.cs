58ca2625efbc4df136a45f77b5a54ecadc817415
famouschamus <chamusg@gmail.com>
2018-02-09 11:47:03 -0500

    added text to MainMenu method

--- Midterm-Project-for-Group/Midterm-Project-for-Group/View.cs
+++ Midterm-Project-for-Group/Midterm-Project-for-Group/View.cs
@@ -1,9 +1,15 @@
-﻿namespace Midterm_Project_for_Group
+﻿using System;
+namespace Midterm_Project_for_Group
 {
     class View
     {
         public static void MainMenu()
         {
+            Console.WriteLine("Here are your  Options");
+            Console.WriteLine("1. View Menu");
+            Console.WriteLine("2. Add menu item to your order");
+            Console.WriteLine("3. Remove menu item from your order");
+            Console.WriteLine("4. Checkout");
 
         }
 
