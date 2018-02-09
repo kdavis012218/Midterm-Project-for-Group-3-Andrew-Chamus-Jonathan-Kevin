b70624f94d813f5bbd118541e6ef04ca25775834
famouschamus <chamusg@gmail.com>
2018-02-09 11:53:25 -0500

    added if statement for MainMenu input

--- Midterm-Project-for-Group/Midterm-Project-for-Group/View.cs
+++ Midterm-Project-for-Group/Midterm-Project-for-Group/View.cs
@@ -10,6 +10,12 @@ namespace Midterm_Project_for_Group
             Console.WriteLine("2. Add menu item to your order");
             Console.WriteLine("3. Remove menu item from your order");
             Console.WriteLine("4. Checkout");
+            bool userChoice = int.TryParse(Console.ReadLine(), out int userC);
+            if (userChoice == false || userC < 1 || userC > 4)
+            {
+                Console.WriteLine("That is not a valid Menu option");
+                MainMenu();
+            }
 
         }
 
