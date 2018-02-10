3d4dee338b577b1a142a468660a69639286819d7
famouschamus <chamusg@gmail.com>
2018-02-09 19:13:09 -0500

    Drew's OrderBasket Class addition

--- Midterm-Project-for-Group/Midterm-Project-for-Group/OrderBasket.cs
+++ Midterm-Project-for-Group/Midterm-Project-for-Group/OrderBasket.cs
@@ -1,6 +1,33 @@
-﻿namespace Midterm_Project_for_Group
+﻿using System;
+using System.Collections.Generic;
+namespace Midterm_Project_for_Group
 {//Andrew
     class OrderBasket
     {
+        public static List<MenuItem> RemoveOrder(List<MenuItem> mcMenu)
+        {
+            bool run = true;
+            while (run == true)
+            {
+                int x = 0;
+                foreach (MenuItem item in mcMenu)
+                {
+                    Console.WriteLine(x + "  " + item);
+                    x++;
+                }
+                Console.WriteLine("Which item would you like to remove?");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int f) && f <= mcMenu.Count)
+                {
+                    mcMenu.RemoveAt(f);
+                    run = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+            return mcMenu;
+        }
     }
 }
