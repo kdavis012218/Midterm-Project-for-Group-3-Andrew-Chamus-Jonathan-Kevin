5b7eea479dbee6833df0d8d90df8d1dcbfc30ae4
famouschamus <chamusg@gmail.com>
2018-02-12 08:36:49 -0500

    added some methods for payment classes

--- Midterm-Project-for-Group/Midterm-Project-for-Group/IPayment.cs
+++ Midterm-Project-for-Group/Midterm-Project-for-Group/IPayment.cs
@@ -1,7 +1,58 @@
-﻿namespace Midterm_Project_for_Group
+﻿using System;
+
+namespace Midterm_Project_for_Group
 {//Kevin
     interface IPayment
     {
+        bool Approval();
+        double CalcAmount();
+    }
+    class CashPayment : IPayment
+    {
+        double amount;
 
+        public bool Approval()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double CalcAmount()
+        {
+            Console.WriteLine("How much would you like to pay?");
+            bool maybePay = double.TryParse(Console.ReadLine(), out double paying);
+            return paying;
+        }
+    }
+    class CardPayment : IPayment
+    {
+        double amount;
+
+        public bool Approval()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double CalcAmount()
+        {
+            Console.WriteLine("How much would you like to pay?");
+            bool maybePay = double.TryParse(Console.ReadLine(), out double paying);
+            return paying;
+        }
+    }
+    class CheckPayment : IPayment
+    {
+        double amount;
+
+        public bool Approval()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double CalcAmount()
+        {
+            Console.WriteLine("How much would you like to pay?");
+            bool maybePay = double.TryParse(Console.ReadLine(), out double paying);
+            return paying;
+        }
     }
 }
